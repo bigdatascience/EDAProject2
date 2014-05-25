d5f491d6794acbdbac3774021e1d33e9a07f1d31
bigdatascience <rupesh.dan@gmail.com>
2014-05-25 18:56:50 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+unzip("./exdata_data_NEI_data.zip", exdir="./")
+
+NEI=readRDS("summarySCC_PM25.rds")
+SCC=readRDS("Source_Classification_Code.rds")
+
+#Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+NEItotbyyear=aggregate(NEI$Emissions ~ NEI$year,FUN=sum)
+colnames(NEItotbyyear)=c("Year","TotalEmissions")
+plot(NEItotbyyear$Year,NEItotbyyear$TotalEmissions,type="l",xlab="Year",ylab="Total Emissions in tons",main="PM2.5")
+dev.copy(png, file="Plot1.png")
+dev.off()
